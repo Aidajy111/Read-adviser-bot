e16eacc7bd291edc1d21137b66baaddc995a0e61
aidas <aidasjy4.00@gmail.com>
2026-03-29 18:23:02 +0800

    lesson 2

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Aidajy111/Read-adviser-bot
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	t := mustToken()
+}
+
+func mustToken() string {
+	token := flag.String("token-bot-token", "", "token for acces to telegram bot")
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is required")
+	}
+}
